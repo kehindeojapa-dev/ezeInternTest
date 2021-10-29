19f5bee77c10a0f56c9e7307f826055f769e0d42
Kehinde <adaeza.dev@gmail.com>
2021-10-29 14:35:02 +0100

    working on the price filter

--- src/Components/PriceFilter.js
+++ src/Components/PriceFilter.js
@@ -1,5 +1,33 @@
+import { useState } from "react";
+
+// Style
+import "./Styles/PriceFilter.css";
+
 function PriceFilter() {
-  return <div>I'm the pricefilter component</div>;
+  const [minValue, setMinValue] = useState();
+  const [maxValue, setMaxValue] = useState();
+
+  return (
+    <div className="pricefilter">
+      <div className="pricefilter__title">Price Filter</div>
+      <div className="pricefilter__minmax">
+        <input
+          type="number"
+          placeholder="Min"
+          min="0"
+          value={minValue}
+          onChange={(e) => setMinValue(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Max"
+          max="2500"
+          value={maxValue}
+          onChange={(e) => setMaxValue(e.target.value)}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default PriceFilter;
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,5 +1,23 @@
+//Style
+import "./Styles/Search.css";
+
 function Search() {
-  return <div>I'm the search component</div>;
+  return (
+    <div className="search">
+      <form className="search__form">
+        <input
+          type="text"
+          placeholder="search for phones(e.g. Iphone Xs, b2, 64gB)"
+          className="search__form__searchInput"
+        />
+        <input
+          type="submit"
+          value="SEARCH"
+          className="search__form__searchBtn"
+        />
+      </form>
+    </div>
+  );
 }
 
 export default Search;
